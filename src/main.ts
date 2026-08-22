4fc989d8f39f642c96b8b17b5728308c29f7ee16
Matheus Almeida <dev.almeida.matheus@gmail.com>
2026-08-22 11:59:45 -0300

    feat: enable CORS for local development

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { API_PREFIX } from './config/common/constants';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   app.setGlobalPrefix(API_PREFIX);
 
   setupSwagger(app);
